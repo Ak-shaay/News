bf0fe85c1259bdff9c9a8acc113b3729aad6cdb3
Akshay <Akshay1999achu@gmail.com>
2022-06-07 13:01:03 +0530

    init

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY="1ab668fe678d4dcba2340eeb0a0bcf5f"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
