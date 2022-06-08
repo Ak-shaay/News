789fd83ce998b4f8792abe37749a066aea2cfa6c
Akshay <Akshay1999achu@gmail.com>
2022-06-08 10:23:13 +0530

    second

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -3,7 +3,9 @@ package com.androiddevs.mvvmnewsapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
@@ -11,8 +13,12 @@ import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
-
     lateinit var viewModel: NewsViewModel
+    private val navHostFragment: NavHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+    }
+
+    private val navController: NavController by lazy { navHostFragment.navController }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,22 @@ class NewsActivity : AppCompatActivity() {
 
         val newsRepository=NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory=NewsViewModelProviderFactory(application,newsRepository)
-        viewModel=ViewModelProvider(owner = this,viewModelProviderFactory).get(NewsViewModel::class.java)
-        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+        viewModel= ViewModelProvider(owner = this,viewModelProviderFactory)[NewsViewModel::class.java]
+        bottomNavigationView.setupWithNavController(navController)
     }
+
+
 }
+
+//    lateinit var viewModel: NewsViewModel
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_news)
+//
+//        val newsRepository=NewsRepository(ArticleDatabase(this))
+//        val viewModelProviderFactory=NewsViewModelProviderFactory(application,newsRepository)
+//        viewModel=ViewModelProvider(owner = this,viewModelProviderFactory).get(NewsViewModel::class.java)
+//        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+//    }
+//}
